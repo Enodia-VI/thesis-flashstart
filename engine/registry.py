58aba2df25b2a04f9855c44c8915f6e9b5724c44
Enodia-VI <vincenzo.daidone98@gmail.com>
2026-09-07 01:32:38 +0200

    Delete engine directory
    Fixing broke push.

--- engine/registry.py
+++
@@ -1,9 +0,0 @@
-from plugins.dns_plugin import DnsPlugin
-from plugins.nicupdate_plugin import NicUpdatePlugin
-
-registry = {
-    "dns_query": DnsPlugin(),
-    "nic_update": NicUpdatePlugin(),
-    # "dot": "",
-    # "doh": "",
-}
